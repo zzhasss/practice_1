e8957bd88bd9d689fb7ddb850a18a2daa157fec6
zzhasss <klipinkonstantin@gmail.com>
2026-06-19 18:23:21 +0300

    Ускоренное решение

--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -6,34 +6,45 @@
 int main() {
     int n;
     long long k;
+
     std::cin >> n >> k;
 
     std::vector<int> p(n);
+
     for (int i = 0; i < n; i++) {
         std::cin >> p[i];
-        p[i]--;
+        --p[i];
     }
 
     std::string s;
     std::cin >> s;
 
-    std::vector<int> rev(n);
+    std::string result(n, ' ');
+    std::vector<bool> used(n, false);
 
     for (int i = 0; i < n; i++) {
-        rev[p[i]] = i;
-    }
+        if (used[i])
+            continue;
 
-    while (k--) {
-        std::string temp(n, ' ');
+        std::vector<int> cycle;
+        int v = i;
 
-        for (int i = 0; i < n; i++) {
-            temp[i] = s[rev[i]];
+        while (!used[v]) {
+            used[v] = true;
+            cycle.push_back(v);
+            v = p[v];
         }
 
-        s = temp;
+        int len = cycle.size();
+        long long shift = k % len;
+
+        for (int i = 0; i < len; i++) {
+            result[cycle[i]] =
+                s[cycle[(i + shift) % len]];
+        }
     }
 
-    std::cout << s << std::endl;
+    std::cout << result << std::endl;
 
     return 0;
 }
