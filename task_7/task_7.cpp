48e31087b21369c727f3c3dad73dc5e6294ba913
zzhasss <klipinkonstantin@gmail.com>
2026-06-22 19:44:07 +0300

    Исправил проверку цифр

--- task_7/task_7.cpp
+++ task_7/task_7.cpp
@@ -6,16 +6,18 @@ int main() {
     std::string s;
     std::cin >> s;
 
-    bool ok = true;
+    if (s.size() != 6) {
+        std::cout << "No";
+        return 0;
+    }
 
-    if (s.size() != 6)
-        ok = false;
+    bool ok = true;
 
     if (!(s[0] >= 'A' && s[0] <= 'Z'))
         ok = false;
 
     for (int i = 1; i <= 3; i++) {
-        if (!(s[i] >= '0' || s[i] <= '9'))
+        if (!(s[i] >= '0' && s[i] <= '9'))
             ok = false;
     }
 
