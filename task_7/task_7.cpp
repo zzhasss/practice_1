f88e83eb61410c37482eb7e49c560b544ae8d696
zzhasss <klipinkonstantin@gmail.com>
2026-06-22 19:37:18 +0300

    Исправил индексацию

--- task_7/task_7.cpp
+++ task_7/task_7.cpp
@@ -15,14 +15,14 @@ int main() {
         ok = false;
 
     for (int i = 1; i <= 3; i++) {
-        if (!(s[i] >= '0' && s[i] <= '9'))
+        if (!(s[i] >= '0' || s[i] <= '9'))
             ok = false;
     }
 
     if (!(s[4] >= 'A' && s[4] <= 'Z'))
         ok = false;
 
-    if (!(s[4] >= 'A' && s[4] <= 'Z'))
+    if (!(s[5] >= 'A' && s[5] <= 'Z'))
         ok = false;
 
     std::cout << (ok ? "Yes" : "No");
