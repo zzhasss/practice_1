ce14cc07d287eda393efd492d04553ad5654f802
zzhasss <klipinkonstantin@gmail.com>
2026-06-27 14:32:41 +0300

    Финальное решение

--- task_11/task_11.py
+++ task_11/task_11.py
@@ -3,22 +3,31 @@ import numpy as np
 
 image = cv2.imread("test2.jpg")
 
-blur = cv2.GaussianBlur(image, (11,11),0)
-hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+blur = cv2.GaussianBlur(image,(11,11),0)
+hsv = cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
 
-mask = cv2.inRange(hsv,
-                   np.array((36,25,25),np.uint8),
-                   np.array((70,255,255),np.uint8))
+mask = cv2.inRange(
+    hsv,
+    np.array((36,25,25),np.uint8),
+    np.array((70,255,255),np.uint8)
+)
 
-contours,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL,
+contours,_ = cv2.findContours(mask,
+                              cv2.RETR_EXTERNAL,
                               cv2.CHAIN_APPROX_SIMPLE)
 
-cnt = contours[0]
+for cnt in contours:
 
-x,y,w,h = cv2.boundingRect(cnt)
+    if cv2.contourArea(cnt) < 100:
+        continue
 
-cv2.circle(image,(x,y),8,(0,0,255),-1)
+    x,y,w,h = cv2.boundingRect(cnt)
 
-cv2.imshow("Img",image)
+    cx = x + w//2
+    cy = y + h//2
+
+    cv2.circle(image,(cx,cy),8,(0,0,255),-1)
+
+cv2.imshow("Result",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
